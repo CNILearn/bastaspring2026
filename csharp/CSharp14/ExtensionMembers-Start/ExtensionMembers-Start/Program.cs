082f609d5da71d467db138f9ea86582489b42a2d
Christian Nagel <christian@christiannagel.com>
2026-03-03 09:21:08 +0100

    Add .NET 10 project with custom LINQ-like extensions
    Created new C# project targeting .NET 10.0. Added Program.cs with a string array and implemented manual Where and Print extension methods in CollectionExtensions. Demonstrated filtering and printing names using these custom extensions.

---
+++ csharp/CSharp14/ExtensionMembers-Start/ExtensionMembers-Start/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] names = ["Jack", "Jochen", "Niki", "Sebastian", "Max", "Juan"];
+
+names.Where(n => n.StartsWith("J")).Print();
+
+
+public static class CollectionExtensions
+{
+    public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        foreach (var item in source)
+        {
+            if (predicate(item))
+                yield return item;
+        }
+    }
+    public static void Print<T>(this IEnumerable<T> source)
+    {
+        foreach (var item in source)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
