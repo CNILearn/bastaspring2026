bd572abff108df85ddfc46ca81fb3bfd0a15da1e
Christian Nagel <christian@christiannagel.com>
2026-03-05 19:03:29 +0100

    BASTA! Source generator

---
+++ sourcegen/BastaSampleGen/ClientApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using BastaSampleGen.Attributes;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(SampleInfo.GetSummary());
+
+[GenerateInfo]
+public class Sample
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Foo() { }
+
+    public void Bar() { }
+}
